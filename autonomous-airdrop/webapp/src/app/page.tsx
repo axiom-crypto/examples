bcd6f15b18a16bf57a6181cc26276d149a984a33
Yu Jiang Tham <yoojbruin@gmail.com>
2023-09-20 18:30:02 -0700

    Update wording on page

--- autonomous-airdrop/webapp/src/app/page.tsx
+++ autonomous-airdrop/webapp/src/app/page.tsx
@@ -35,11 +35,11 @@ export default async function Home({ searchParams }: PageProps) {
   return (
     <>
       <Title>
-        UselessToken Autonomous Airdrop
+        Autonomous Airdrop
       </Title>
       <div className="text-center">
         Anyone who has used <Link href="https://app.uniswap.org/swap" target="_blank">Uniswap</Link> on Goerli testnet 
-        after Goerli block 9000000 is eligible for a UselessToken airdrop of 100 UT. You may need to wait a few minutes 
+        after Goerli block 9000000 is eligible for an airdrop of a newly deployed test token called UselessToken. You may need to wait a few minutes 
         after executing your swap for the indexer to pick it up.
       </div>
       { renderButton() }
