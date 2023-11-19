2fa340246317b3d657fba82dd4ead08adb02c34e
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2023-11-18 17:24:08 -0800

    feat: dev example

--- node-example/node/src/circuit/circuit.ts
+++ node-example/node/src/circuit/circuit.ts
@@ -9,13 +9,15 @@ import {
 /// They will be auto-parsed into `MyCircuitInputs` type.
 export interface MyInputs {
   blockNumber: number;
-  rawSlot: string; // should be bytes32
+  keystoreContractAddress: string; // address
+  verificationKey: string; // address
 }
 
 /// These should be the _variable_ inputs to your circuit. Constants can be hard-coded into the circuit itself (see below).
 export interface MyCircuitInputs {
+  verificationKey: CircuitValue;
+  keystoreContractAddress: CircuitValue;
   blockNumber: CircuitValue;
-  rawSlot: CircuitValue256;
 }
 
 export type MyCircuitCode = (
@@ -62,24 +64,21 @@ export const myCircuitFn: MyCircuitCode = async (
   //
   // docs.axiom.xyz/axiom-repl/axiom-repl
   //
-  const { blockNumber, rawSlot } = myInputs;
+  const { verificationKey, keystoreContractAddress, blockNumber } = myInputs;
 
-  // I only want to look at a single address in my circuit, so I hardcode it here:
-  const address = "0xBd5307B0Bf573E3F2864Af960167b24Aa346952b"; // AxiomV2Query
+  // fetch the verification key storage data
+  const storage = getStorage(blockNumber, keystoreContractAddress);
+  // fetch the verification key storage value
+  const slotValue = storage.slot(3);
+  const vfKeyCv = slotValue.toCircuitValue();
 
-  // Since the blockNumber is a variable input, let's add it to the results that will be sent to my callback function:
+  // assert that the value of verification key on-chain is the same
+  checkEqual(vfKeyCv, verificationKey);
+
+  // add the verificationKey and blockNumber to the callback, for it to be passed
+  // as a result to the callback client contract
+  addToCallback(verificationKey);
   addToCallback(blockNumber);
-  // We are going to need to validate this rawSlot in the smart contract
-  addToCallback(rawSlot);
-
-  // Make a data subquery request to get the storage data:
-  // Note: this function will know that `address` is fixed because it's a string and not a CircuitValue
-  const storage = getStorage(blockNumber, address);
-  // access the value at storage slot `slot`
-  const slotVal = storage.slot(rawSlot);
-  // I got what I wanted (the slot value), adding it to callback:
-  addToCallback(slotVal);
-
   // If I wanted to do additional computations on the results I got above, I'd do it here.
 
   // This is the end of the circuit!
--- node-example/node/src/index.ts
+++ node-example/node/src/index.ts
@@ -21,54 +21,10 @@ const axiom = new Axiom({
 });
 
 const defaultInputs: MyInputs = {
-  blockNumber: 10023211,
-  rawSlot: ZeroHash,
+  verificationKey: "0x1E27090e6842a20b3dAF4621AFD20D44479d780b",
+  keystoreContractAddress: "0x05870fA27656036ecD754667edD2e91f81590f39",
+  blockNumber: 10067126,
 };
-
-/// **Assuming** that your `key` is of uint type so it is left padded to 32 bytes.
-function getRawSlotForMapping(
-  mappingSlot: HexString,
-  key: HexString,
-  offset: number,
-): HexString {
-  // See https://docs.soliditylang.org/en/v0.8.23/internals/layout_in_storage.html#mappings-and-dynamic-arrays
-  const p = ethers.zeroPadValue(mappingSlot, 32);
-  const k = ethers.zeroPadValue(key, 32);
-  const slot = keccak256(ethers.concat([k, p]));
-  const offsetSlot = ethers.toBigInt(slot) + BigInt(offset);
-  return ethers.toBeHex(offsetSlot, 32);
-}
-
-async function generateQueryWithExtraData(
-  loadedCircuit: MyAxiomCircuit,
-  blockNumber: number,
-  mappingSlot: HexString,
-  key: HexString,
-  offset: number,
-  callbackAddr: string,
-): Promise<{
-  query: QueryBuilderV2;
-  builtQuery: BuiltQueryV2;
-  payment: string;
-}> {
-  // This is a calculation purely in Typescript. It cannot be trusted since it's outside the circuit!
-  const offsetSlot = getRawSlotForMapping(mappingSlot, key, offset);
-  // We pass the inputs of the `getRawSlotForMapping` as extra data to the callback contract. In that contract we will perform the same function in Solidity to verify that this part of the calculation was done correctly.
-  const callback = {
-    target: callbackAddr,
-    extraData: ethers.concat([
-      bytes32(mappingSlot),
-      bytes32(key),
-      bytes32(offset),
-    ]),
-  };
-  const input = {
-    blockNumber,
-    rawSlot: offsetSlot,
-  };
-  await loadedCircuit.run(input);
-  return loadedCircuit.generateQuery(axiom, callback);
-}
 
 const axiomMain = async () => {
   const customCircuit = new MyAxiomCircuit(
@@ -84,43 +40,7 @@ const axiomMain = async () => {
 
   // In production, you should save the proving key and verifying key to disk and build from the cached proving key by calling `customCircuit.loadPrebuilt(config, pkey, vkey)`.
 
-  // The following part creates a ZK proof from a variable input and builds the query for you to send on-chain.
-  // We'll just do an example input here:
-  const test = {
-    blockNumber: 10051661,
-    // To find the slot of a mapping, use the following command:
-    // $ forge inspect <contract> storage-layout --pretty
-    mappingSlot: ethers.toBeHex(300), // slot 300 of AxiomV2Query is `mapping(uint256 => AxiomQueryMetadata) public queries`
-    queryId:
-      66966676587627506390515603527468953886054461497745819784183473907733311885447n,
-    offset: 1, // offset 1 should be the `payment` field in `AxiomQueryMetadata`
-  };
-  const { query, builtQuery, payment } = await generateQueryWithExtraData(
-    customCircuit,
-    test.blockNumber,
-    test.mappingSlot,
-    ethers.toBeHex(test.queryId, 32),
-    test.offset,
-    "0x4737243a3134e0af5cd8a8eAe457B4A1706FD411",
-  );
-  console.log("Query built with the following params:", builtQuery);
-
-  console.log(
-    "Sending a Query to AxiomV2QueryMock with payment amount (wei):",
-    payment,
-  );
-
-  const queryId = await query.sendOnchainQuery(
-    payment,
-    (receipt: ethers.ContractTransactionReceipt) => {
-      // You can do something here once you've received the receipt
-      console.log("receipt", receipt);
-    },
-  );
-  console.log(
-    "View your Query on Axiom Explorer:",
-    `https://explorer.axiom.xyz/v2/goerli/mock/query/${queryId}`,
-  );
+  await customCircuit.run(defaultInputs);
 };
 
 axiomMain();
